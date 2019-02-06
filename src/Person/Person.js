fd6dae1fa79308b1bcfd5360d34ae28e4598ee6a
xjanus <xjanus.official@gmail.com>
2019-02-06 17:32:05 +0200

    heiiii

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
 
 
     return(
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name}. 
             I am {props.age} years old.</p>
             <p>{props.children}</p>
