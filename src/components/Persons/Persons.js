03bc60ccb9f3be4c8292bbb2694cb70243ff72f6
xjanus <xjanus.official@gmail.com>
2019-02-07 16:00:17 +0200

    submit now

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Person from './Person/Person'
+
+//props.persons is - this.state.persons - array
+//props.clicked is - this.deletePersonHandler - function reference
+//props.changed is - this.nameChangeHandler - function reference
+const persons = (props) =>  props.persons.map((person, index) => {
+        return <Person 
+          click = {() => props.clicked(index)}
+          name  = {person.name} 
+          age   = {person.age}
+          key = {person.id}
+          changed = {(event) => props.changed(event, person.id)} />
+      });
+
+      export default persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   // state can only be accessed in class-based components!
@@ -56,49 +57,28 @@ class App extends Component {
   render() {
     // Needs to be implemented in class-based components! Needs to return some JSX!
 
-    let persons = null;
-    let btnClass = "";
+    let persons = null;    
 
     if(this.state.showPersons) {  
-      
-      btnClass = classes.Red;
-      
       persons = (
           <div>
-                {this.state.persons.map((person, index) => {
-                  return <Person 
-                    click = {this.deletePersonHandler.bind(this, index)}
-                    name  = {person.name} 
-                    age   = {person.age}
-                    key = {person.id}
-                    changed = {(event) => this.nameChangeHandler(event, person.id)} />
-                })
-                }
+            <Persons 
+              persons={this.state.persons}
+              clicked={this.deletePersonHandler}
+              changed={this.nameChangeHandler} />
           </div>
               );
       }
-
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2){
-      assignedClasses.push(classes.red);
-    }
-    if(this.state.persons.length <= 1){
-      assignedClasses.push(classes.bold);
-    }
 
   // ***************** RETURN *************************************
 
     return (
         <div className={classes.App}>
         
-          <h1> Hi! I'm a React App.</h1>
-          <p className={assignedClasses.join(' ')}>This is really working!!!</p>
-          
-          <button 
-            className={btnClass}
-            onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
+          <Cockpit 
+            showPersons={this.state.showPersons} 
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler} />
 
           {persons}
         
