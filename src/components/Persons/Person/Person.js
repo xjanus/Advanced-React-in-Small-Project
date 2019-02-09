023d76d652a92f9b7c65e920747a32177247b468
xjanus <xjanus.official@gmail.com>
2019-02-09 18:05:18 +0200

    hei

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,19 +9,20 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
     //*****************************************************/
 
-        return(
+        return (
             <div className={classes.Person}>
                 
                 <p onClick={this.props.click}>
-                I'm a {this.props.name}. 
-                I am {this.props.age} years old.
+                    I'm a {this.props.name}. 
+                    I am {this.props.age} years old.
                 </p>
                 
                 <p>{this.props.children}</p>
                 
-                <input type="text" 
-                onChange={this.props.changed} 
-                value={this.props.name}>
+                <input 
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}>
                 </input>
     
             </div>
